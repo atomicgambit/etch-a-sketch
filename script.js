def6363c54f5823f2c97278d24419f65941803f5
Vito <134098951+atomicgambit@users.noreply.github.com>
2024-01-06 10:13:39 +0100

    Adds 16 vertical cells

---
+++ script.js
@@ -0,0 +1,18 @@
+//Creating the grid
+//Add a container in your html
+//Create a reference in js to the grid
+//Use a for loop within a for loop to create a grid of 16 by 16
+//let i = 0, i < 16, i++
+// let j = 0, i < 16, i++
+//create a div element, call it cell
+//add a class to cell
+//append class to grid
+//give the cells a dark border
+
+const grid = document.querySelector(".grid");
+for (let i = 0; i < 16; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  grid.appendChild(cell);
+}
+console.log("hello");

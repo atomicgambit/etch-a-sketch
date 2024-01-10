e80807d4495f900825ae0ce11fa255592f67d98f
Vito <134098951+atomicgambit@users.noreply.github.com>
2024-01-10 07:10:38 +0100

    adds button that changes the size of the grid

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 //Create the grid
 
 const grid = document.querySelector(".grid");
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  grid.appendChild(row);
-  for (let i = 0; i < 16; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    row.appendChild(cell);
+let gridSize = 16;
+
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    grid.appendChild(row);
+    for (let i = 0; i < size; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      row.appendChild(cell);
+    }
   }
 }
+
+createGrid(gridSize);
 
 //Draws on mousedown
 
@@ -25,4 +31,12 @@ grid.addEventListener("mousedown", () => {
 grid.addEventListener("mouseup", () => {
   const allCells = document.querySelectorAll(".cell");
   allCells.forEach((cell) => cell.removeEventListener("mouseenter", colorCell));
+});
+
+//Create new grid with btn
+
+const gridBtn = document.querySelector("#gridBtn");
+gridBtn.addEventListener("click", () => {
+  grid.textContent = "";
+  createGrid(32);
 });
